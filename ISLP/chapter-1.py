667210b894144ce9434d6f88b2b2aa483ec57e56
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-02-05 05:21:16 +0530

    first chapter done

---
+++ ISLP/chapter-1.py
@@ -0,0 +1,7 @@
+# So the first chaptere was just an introduction to the book 
+
+# 1. It covers little info about the two datasets i.e is wage and NCI60 
+# 2. there are 3 types of problem : a. Regression b. Classification c. Clustering
+# 3. n stands for number of sample and p stands for number of features
+# 4. the power of R.. supose we a matrix of n x p then the power of R is n x p
+# 5. the book is about the prediction and the inference
